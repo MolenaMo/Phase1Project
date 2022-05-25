21110b74fc052a3c55ad54b3b07548e8dfb70ef8
MLM <100675603+MolenaMo@users.noreply.github.com>
2022-05-25 05:35:20 -0400

    updated

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 document.addEventListener("DOMContentLoaded", (e) =>{
     // alert("I have loaded and parse!")--worked
 
-    //Title creation and Text
-  let title = document.createElement('h1')
-  document.querySelector('header').appendChild(title)
-    title.textContent = "Activity Bot"
-    title.addEventListener("mouseover", (e) =>{
-     e.target.style.color = "white";}
-    )
-   
+
+    //h1, text, & mouse event creation
+  let h1 = document.createElement('h1')
+  document.querySelector('header').appendChild(h1)
+    h1.textContent = "Activity Bot"
+    h1.addEventListener("mouseover", (e) =>{
+        e.target.style.color = "#cedbdd";
+    setTimeout (function(){
+        e.target.style.color = "";
+        }, 500);
+        }, false);
+      
 //placeholder for activity text to display
-    let actHolder =  document.createElement('h4')
-    document.getElementById("activity").appendChild(actHolder)
-   
+    let actHolder = document.createElement('h4')
+    document.getElementById("activity").append(actHolder)
+    //activity button Event
+    document.getElementById('btn').addEventListener('mouseenter',(e) =>{
+        e.target.style.color = "blue";
+
+    } )
+
     document.getElementById('btn').addEventListener("click", (e) =>{
         // console.log("I Work")--worked
     fetch("https://apis.scrimba.com/bored/api/activity")
@@ -22,6 +31,6 @@ document.addEventListener("DOMContentLoaded", (e) =>{
        actHolder.textContent = data.activity
 
     })
- 
+  
 })})
 
